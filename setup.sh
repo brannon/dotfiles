f063dce2337d0dc47e7b809525b293b106bb4f6a
brannon <brannon@iterator.net>
2022-04-04 14:26:54 -0600

    Update setup.sh to pass the --non-interactive option.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 export DOTFILES_REPO=$(dirname $0)
 
 if [ $(uname) == 'Linux' ]; then
-    $DOTFILES_REPO/linux/bootstrap.sh --silent --no-clone 2>&1 | tee $HOME/.setup.log
+    $DOTFILES_REPO/linux/bootstrap.sh --non-interactive --no-clone 2>&1 | tee $HOME/.setup.log
 else
     echo "Unexpected platform: $(uname)"
     exit 1
