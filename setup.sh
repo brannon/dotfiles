dd7a8df0bd0e63d96c61168f54bc9c3c0ae5e75d
brannon <brannon@iterator.net>
2022-04-04 14:25:55 -0600

    Update setup.sh to detect the location of the repo and bootstrap scripts

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+export DOTFILES_REPO=$(dirname $0)
+
 if [ $(uname) == 'Linux' ]; then
-    linux/bootstrap.sh --silent --no-clone 2>&1 | tee $HOME/.setup.log
+    $DOTFILES_REPO/linux/bootstrap.sh --silent --no-clone 2>&1 | tee $HOME/.setup.log
 else
     echo "Unexpected platform: $(uname)"
     exit 1
